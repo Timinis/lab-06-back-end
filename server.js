59bbd65b37e177620d16eec005ac1db7652dfc0d
Timinis <litimc3@gmail.com>
2018-09-18 13:11:48 -0700

    added map

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ const superagent = require('superagent');
 const cors = require('cors');
 const app = express();
 
-// ??
 app.use(cors());
 
 require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get('/location', (request, response) => {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${
     request.query.data
   }&key=${process.env.GOOGLE_API_KEY}`;
   return superagent.get(url).then(result => {
+    console.log(result.body);
     const locationResult = {
       search_query: request.query.data,
       formatted_query: result.body.results[0].formatted_address,
-      latitude: result.body.result[0].geometry.location.lat,
-      longtitude: result.body.result[0].geometry.location.lng
+      latitude: result.body.results[0].geometry.location.lat,
+      longtitude: result.body.results[0].geometry.location.lng
     };
     response.send(locationResult);
   });
